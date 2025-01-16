b059992a92a876edb0772acb0d2de440b7a2bf10
Ethan Chen <echen@lcs.on.ca>
2025-01-16 14:32:25 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-16.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
